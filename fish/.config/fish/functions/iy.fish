554968d3865c4707f27b32eaf5f9043188575853
mnojz <manoj.joshi.public@gmail.com>
2026-08-05 08:58:03 +0545

    up

---
+++ fish/.config/fish/functions/iy.fish
@@ -0,0 +1,3 @@
+function iy --wraps='yay -Sy --noconfirm ' --description 'alias iy=yay -Sy --noconfirm '
+    yay -Sy --noconfirm  $argv
+end
--- hyprland/.config/hypr/modules/autostart.lua
+++ hyprland/.config/hypr/modules/autostart.lua
@@ -1,7 +1,8 @@
 ---- AUTOSTART ----
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd("ambxst")
-  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-  hl.exec_cmd("kdeconnect & kdeconnect-indicator")
+  hl.exec_cmd("sh -c 'ambxst >/dev/null 2>&1 &'")
+  hl.exec_cmd("sh -c '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &'")
+  hl.exec_cmd("sh -c 'kdeconnect >/dev/null 2>&1 &'")
+  hl.exec_cmd("sh -c 'kdeconnect-indicator >/dev/null 2>&1 &'")
 end)
