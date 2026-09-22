3a210fde978884d8cd393a5befb582a2acc2ae8e
mnojz <manoj.joshi.public@gmail.com>
2026-09-22 10:14:06 +0545

    fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,10 @@
-# this will disable greeting message
+# Show fastfetch only when the terminal is large enough
 function fish_greeting
-fetch
+    if test (tput cols) -ge 70 -a (tput lines) -ge 17
+        fastfetch
+    else if test (tput cols) -ge 17 -a (tput lines) -ge 9
+        palette
+    end
 end
 
 starship init fish | source
@@ -14,5 +18,3 @@ set -x QT_QPA_PLATFORMTHEME qt5ct
 
 set -x PATH /opt/cuda/bin $PATH
 set -x LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
-
-thefuck --alias | source
